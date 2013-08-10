064558256f7c2e6a36bf52f12bf7d128a8aedd8d
pcdinh <pcdinh@gmail.com>
2013-08-10 16:32:51 +0700

    Fixed missing LICENSE.txt when running pip install

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+
 import os
 import sys
 from distutils.core import setup
 
 # See: http://docs.python.org/3.1/distutils/uploading.html
+# .pypirc file should look like:
+# [pypirc]
+#   servers = pypi
+#   [server-login]
+#     username:my_username
+#     password:my_password
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload -r pypi')
     sys.exit()
@@ -10,10 +18,14 @@ if sys.argv[-1] == 'publish':
 setup(
     name='greenclock',
     packages=['greenclock'],
-    version='0.1.0',
+    version='0.1.1',
     description='gevent-based task scheduling library.',
     license=open('LICENSE.txt').read(),
     long_description=open("README.md").read(),
+    data_files=[("", ["LICENSE.txt"])],
+    install_requires=[
+        'gevent',
+    ],
     author='Pham Cong Dinh',
     author_email='pcdinh@gmail.com',
     url='https://github.com/pcdinh/greenclock',
@@ -28,6 +40,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Natural Language :: English',
-        'Topic :: Software Development :: Libraries'
+        'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
