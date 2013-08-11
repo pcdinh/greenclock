f191e3beeeeaab009b0a8e393b8338008c8aebfd
pcdinh <pcdinh@gmail.com>
2013-08-11 16:59:21 +0700

    Bump version to 0.3.0

--- greenclock/version.py
+++ greenclock/version.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # license: MIT
-version_info = (0, 2, 1, 'dev', None)
+version_info = (0, 3, 0, 'dev', None)
 __title__ = 'greenclock'
-__version__ = '0.2.1-dev'
+__version__ = '0.3.0-dev'
 __author__ = 'pcdinh'
 __license__ = 'MIT'
