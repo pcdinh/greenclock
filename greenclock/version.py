57520fbdf1a8c7e912046257efb92b24979142f8
pcdinh <pcdinh@gmail.com>
2020-03-03 17:51:53 +0700

    Pump version

--- greenclock/version.py
+++ greenclock/version.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # license: MIT
-version_info = (0, 3, 0, 'final', None)
+version_info = (0, 3, 1, 'final', None)
 __title__ = 'greenclock'
-__version__ = '0.3.0-final'
+__version__ = '0.3.1-final'
 __author__ = 'pcdinh'
 __license__ = 'MIT'
