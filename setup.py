0cc92b746de37f3d5288d607f06a98af962e1495
pcdinh <pcdinh@gmail.com>
2020-03-03 16:15:39 +0700

    Fixed: UserWarning: Unknown distribution option: 'install_requires'

--- setup.py
+++ setup.py
@@ -2,37 +2,31 @@
 
 import os
 import sys
-from distutils.core import setup
+from setuptools import setup
 
 from greenclock.version import __version__
 
-# See: http://docs.python.org/3.1/distutils/uploading.html
-# .pypirc file should look like:
-# [pypirc]
-#   servers = pypi
-#   [server-login]
-#     username:my_username
-#     password:my_password
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload -r pypi')
     sys.exit()
 
-VERSION = __version__[:__version__.index('-')]
+VERSION = __version__[:__version__.index('.')]
+
 
 def long_description():
     """Get the long description from the README"""
     return open(os.path.join(sys.path[0], 'README.md')).read()
+
 
 setup(
     name='greenclock',
     packages=['greenclock'],
     version=VERSION,
     description='A library that provides time-based task scheduling using green threads via gevent.',
-    # LICENSE issue: https://github.com/josegonzalez/beaver/commit/1878b8a09b5e308d182f59def64b451f8ce4232d
     license='MIT',
     long_description=long_description(),
     install_requires=[
-        'gevent',
+        'gevent>=1.5a4',
     ],
     author='Pham Cong Dinh',
     author_email='pcdinh@gmail.com',
@@ -46,6 +40,8 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Natural Language :: English',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
